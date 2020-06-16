21e19f817f2a952065a0858a883cd4cf51dc020b
souvikban16 <59077658+souvikban16@users.noreply.github.com>
2020-06-16 22:00:37 +0530

    Added More functionality
    Can now open cmd and build and run cpp and python files. Also currently can only open already created files.
    Will implement creating new files, and saving in the next commit

--- vik.py
+++ vik.py
@@ -1,6 +1,7 @@
 import os 
 import tkinter as tk
-
+from tkinter import filedialog
+from tkinter import messagebox
 
 class Menubar:
 
@@ -10,9 +11,14 @@ class Menubar:
 		menubar.add_command(label = "New", command = parent.newFile)
 		menubar.add_command(label = "Save", command = parent.saveFile)
 		menubar.add_command(label = "Save As", command = parent.saveAsFile)
-		menubar.add_command(label = "Open", command = openFile)
+		menubar.add_command(label = "Open", command = parent.openFile)
+		menubar.add_command(label = "About", command = parent.showAbout)
 		menubar.add_separator()
-		menubar.add_command(label = "CMD", command = openCMD)
+		menubar.add_separator()
+		menubar.add_separator()
+		menubar.add_command(label = "CMD", command = parent.openCMD)
+		menubar.add_command(label = "Build", command = parent.buildFile)
+		menubar.add_command(label = "Run", command = parent.runFile)
 
 class Vik:
 
@@ -21,7 +27,11 @@ class Vik:
 		master.geometry("500x500")
 
 		self.master = master
-
+		self.filepath = None
+		self.filename = None
+		self.filenameonly = None
+		self.cwd = None
+		self.filetype = None
 		self.textArea = tk.Text(master)
 		self.scrollBarY = tk.Scrollbar(master, command = self.textArea.yview)
 		# self.scrollBarX = tk.Scrollbar(master, command = self.textArea.xview)
@@ -33,7 +43,74 @@ class Vik:
 
 		self.menu = Menubar(self)
 
+	def newFile(self):
 
+		if self.filepath:
+			result = messagebox.askyesnocancel(title = "Alert!", message = "Save File first ? ")
+			print(result)
+			if result == None:
+				return
+			elif result == True:
+				print("saved")
+		self.filepath = None
+		self.filename = None
+		self.filetype = None
+		self.filenameonly = None
+		self.master.title("Untitled -- Vik Text Editor")
+		self.textArea.delete(1.0, tk.END)
+	def saveFile(self):
+		pass
+	def saveAsFile(self):
+		pass
+	def openFile(self):
+		self.filepath = filedialog.askopenfilename(defaultextension = "*.*")
+		print(self.filepath)
+		if self.filepath:
+			self.textArea.delete(1.0, tk.END)
+			with open(self.filepath, "r") as file:
+				self.textArea.insert(1.0, file.read())
+		self.master.title(self.filepath + " -- Vik Text Editor")
+		index = self.filepath.rfind("/")
+		self.filename = self.filepath[index + 1 :]
+		self.cwd = self.filepath[:index]
+		dotindex = self.filepath.rfind(".")
+		self.filetype = self.filepath[dotindex + 1 :]
+		self.filenameonly = self.filepath[index + 1:dotindex]
+		print(self.filepath)
+		print(self.cwd)
+		print(self.filename)
+		print(self.filetype)
+
+
+	def showAbout(self):
+		messagebox.showinfo(title = "About Vik Text Editor", message = "I am Souvik Banerjee and I have made this editor for fun!")
+		
+	def openCMD(self):
+		if not self.filepath:
+			messagebox.showerror(title = "Alert!", message = "Save file first!")
+		else:
+			os.system("start cmd /k cd " + self.cwd)
+	def buildFile(self):
+		if not self.filepath:
+			messagebox.showerror(title = "Alert!", message = "Save file first!")
+		else:
+			if self.filetype == "py":
+				temp = 'start cmd /k \"cd '+ self.cwd + ' && python ' + self.filename +"\""
+				os.system(temp)
+			elif self.filetype == "cpp":
+				temp = 'start cmd /k \"cd '+ self.cwd + ' && g++ ' + self.filename + " -o " + self.filenameonly + "\""
+				os.system(temp)
+				print(temp)
+	def runFile(self):
+		if not self.filepath:
+			messagebox.showerror(title = "Alert!", message = "Save file first!")
+		else:
+			if self.filetype == "py":
+				temp = 'start cmd /k \"cd '+ self.cwd + ' && python ' + self.filename +" \""
+				os.system(temp)
+			elif self.filetype == "cpp":
+				temp  = 'start cmd /k \"cd '+ self.cwd + ' && ' + self.filenameonly +"\""
+				os.system(temp)
 
 
 
